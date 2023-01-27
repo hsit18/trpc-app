8a35242e8e90d97a055bf6376fbf9a701c739e63
Harpreet Singh <hsit18@gmail.com>
2023-01-27 23:12:49 +0530

    open api issue check

--- src/route/user.route.ts
+++ src/route/user.route.ts
@@ -17,8 +17,16 @@ export const userRouter = router({
             return user;
         }),
     create: publicProcedure
+        .meta({
+            openapi: {
+                method: 'POST',
+                path: '/user/create',
+                tags: ['users'],
+                summary: 'Create new user',
+            },
+        })
         .input(z.object({ name: z.string().min(5), email: z.string().email() }))
-        .output(z.any())
+        .output(z.object({ id: z.string().uuid(), name: z.string().nullable(), email: z.string().email() }))
         .mutation(async (req) => {
             const user = await req.ctx.prisma.user.create({
                 data: req.input,
